933603716ec687a6a59ef076a9e064f156bb697d
Umair Jibran <umairjibran7@gmail.com>
2021-07-28 10:17:24 +0500

    code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'cubit/counter_cubit.dart';
+import 'business_logic/cubit/counter_cubit.dart';
+import 'presentation/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,103 +15,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.teal,
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncremented!)
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("Counter incremented"),
-                      duration: Duration(milliseconds: 500),
-                    ),
-                  );
-                else
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("Counter decremented"),
-                      duration: Duration(milliseconds: 500),
-                    ),
-                  );
-              },
-              builder: (context, state) {
-                if (state.counterValue == 0) {
-                  return Text(
-                    "Could it be nothing?",
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-                if (state.counterValue < 0) {
-                  return Text(
-                    "🧐 " + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-                if (state.counterValue % 5 == 0) {
-                  return Text(
-                    "Hmm, Multiple of 5!!",
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-                return Text(
-                  (state.counterValue % 2 == 0 ? "Even: " : "Odd: ") +
-                      state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            Row(
-              children: [
-                Spacer(flex: 2),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: Icon(Icons.remove),
-                ),
-                Spacer(),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
-                ),
-                Spacer(flex: 2),
-              ],
-            ),
-          ],
-        ),
+        home: MyHomePage(title: 'Flutter Counter via CUBIT'),
       ),
     );
   }
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../business_logic/cubit/counter_cubit.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented!)
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Counter incremented"),
+                      duration: Duration(milliseconds: 500),
+                    ),
+                  );
+                else
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Counter decremented"),
+                      duration: Duration(milliseconds: 500),
+                    ),
+                  );
+              },
+              builder: (context, state) {
+                if (state.counterValue == 0) {
+                  return Text(
+                    "Could it be nothing?",
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+                if (state.counterValue < 0) {
+                  return Text(
+                    "🧐 " + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+                if (state.counterValue % 5 == 0) {
+                  return Text(
+                    "Hmm, Multiple of 5!!",
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+                return Text(
+                  (state.counterValue % 2 == 0 ? "Even: " : "Odd: ") +
+                      state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Row(
+              children: [
+                Spacer(flex: 2),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                Spacer(),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+                Spacer(flex: 2),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
