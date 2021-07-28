d33f1725ea8b852b78224c755b9a0448499dcb95
Umair Jibran <umairjibran7@gmail.com>
2021-07-28 11:19:18 +0500

    convert imports to relative

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:learning_flutter_bloc/presentation/screens/second_screen.dart';
-import 'package:learning_flutter_bloc/presentation/screens/third_screen.dart';
+import 'presentation/screens/second_screen.dart';
+import 'presentation/screens/third_screen.dart';
 import 'business_logic/cubit/counter_cubit.dart';
 import 'presentation/screens/home_screen.dart';
 
